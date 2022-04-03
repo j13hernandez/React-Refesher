5cb0cf8373b74235ec23164d19eca43f2704e4ae
Palma <1hernandezj3@gmail.com>
2022-04-02 21:59:37 -0400

    "Create a hardcoded list"

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GoalList from './components/GoalList';
+
+function App() {
+  return (
+    <div className='course-goal'>
+      <h2>Course Goals</h2>
+      <GoalList />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/GoalList.js
@@ -0,0 +1,13 @@
+import './GoalList.css';
+
+const GoalList = () => {
+  return (
+    <ul className='goal-list'>
+      <li>Understand React better</li>
+      <li>Understand JS better</li>
+      <li>Create a cool mobile app</li>
+    </ul>
+  );
+};
+
+export default GoalList;
